cb56dd7846d47b24406516c8189f6bd710695de6
Tapo4ek-exe <tapo4ek.exe@gmail.com>
2024-02-19 23:36:32 +0300

    Добавлены настройки для подключения к БД

---
+++ product-api/src/main/java/ru/emarket/productapi/configuration/AppConfig.java
@@ -0,0 +1,9 @@
+package ru.emarket.productapi.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.emarket.productapi")
+public class AppConfig {
+}
